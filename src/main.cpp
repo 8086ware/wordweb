81434e31efe3819bd4b0da53588363de14da0af5
8086ware <kirillos.khan08@outlook.com>
2026-06-29 00:35:15 -0400

    Declare variables and stuff in classes

--- src/main.cpp
+++ src/main.cpp
@@ -7,30 +7,40 @@ class Wordweb : public wxApp
 {
 	class GUI_Panel : public wxPanel
 	{
+		wxBoxSizer* sizer {new wxBoxSizer{wxHORIZONTAL}};
+
+		wxTextCtrl* input_win{new wxTextCtrl{this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_CHARWRAP | wxTE_MULTILINE | wxTE_PROCESS_ENTER}};
+		wxTextCtrl* chat_win{new wxTextCtrl{this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_CHARWRAP | wxTE_MULTILINE | wxTE_READONLY}};
+		wxListBox* name_win{new wxListBox{this, wxID_ANY}};
+
+		bool connected{};
 		wxSocketClient* client{new wxSocketClient};
 
 		void process_input_box(wxCommandEvent& event);
 
-		public:
-			GUI_Panel(wxWindow* parent);
+	public:
+		GUI_Panel(wxWindow* parent);
 	};
 
 	class GUI_Frame : public wxFrame
 	{
-		public:
-			GUI_Frame();
-	};
+		wxNotebook* notebook {new wxNotebook{this, wxID_ANY}};
+		GUI_Panel* default_panel {new GUI_Panel{notebook}};
 
 	public:
-		bool OnInit() override;
+		GUI_Frame();
+	};
+
+	GUI_Frame* frame;
+public:
+	bool OnInit() override;
 };
 
 wxIMPLEMENT_APP(Wordweb);
 
 bool Wordweb::OnInit()
 {
-	GUI_Frame* frame {new GUI_Frame};
-
+	frame = new GUI_Frame;
 	frame->Show();
 
 	return true;
@@ -38,21 +48,11 @@ bool Wordweb::OnInit()
 
 Wordweb::GUI_Frame::GUI_Frame() : wxFrame(nullptr, wxID_ANY, "Wordweb") // The actual wxwidgets window
 {
-	wxNotebook* notebook {new wxNotebook{this, wxID_ANY}};
-
-	GUI_Panel* default_panel {new GUI_Panel{notebook}};
-
 	notebook->AddPage(default_panel, "None");
 }
 
 Wordweb::GUI_Panel::GUI_Panel(wxWindow* parent) : wxPanel(parent, wxID_ANY) // The window containing chat/input/name list. It also contains the socket, and other information that is connected to the chat server.
 {
-	wxBoxSizer* sizer {new wxBoxSizer{wxHORIZONTAL}};
-
-	wxTextCtrl* input_win{new wxTextCtrl{this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_CHARWRAP | wxTE_MULTILINE | wxTE_PROCESS_ENTER}};
-	wxTextCtrl* chat_win{new wxTextCtrl{this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_CHARWRAP | wxTE_MULTILINE | wxTE_READONLY}};
-	wxListBox* name_win{new wxListBox{this, wxID_ANY}};
-
 	sizer->Add(input_win, 2, wxEXPAND);
 	sizer->Add(chat_win, 5, wxEXPAND);
 	sizer->Add(name_win, 1, wxEXPAND);
